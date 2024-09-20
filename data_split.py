570a689111c5ba71201e122013641ce6b72ae0e0
Xi Wang <wangxieric@gmail.com>
2024-09-20 09:22:14 +0100

    Update data_split.py

--- data_split.py
+++ data_split.py
@@ -13,13 +13,13 @@ ds = load_dataset("monology/pile-uncopyrighted", split="train")
 flatten_ds = ds.map(flatten_metadata)
 
 # Convert the dataset to a pandas DataFrame
-df = flatten_ds.to_pandas()  # Use the appropriate split, e.g., "train", "test"
+df = flatten_ds.set_format(type='pandas', columns=['text', 'pile_set_name'])  # Use the appropriate split, e.g., "train", "test"
 
 # Step 2: Split dataset based on the pile_set_name in the metadata
 groups = df.groupby(df['pile_set_name'])
 
 # Step 3: Save each split to a CSV file
 for pile_set_name, group in groups:
-    group.to_csv(f'{pile_set_name}.csv', index=False)
+    group.to_csv(f'/mnt/parscratch/users/ac1xwa/pythia/pre-train_data/{pile_set_name}.csv', index=False)
 
 print("Dataset split and saved to CSV files.")
