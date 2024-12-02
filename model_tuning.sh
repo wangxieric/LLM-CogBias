92804679af50d0ae8404edc514173a26a6e8753d
Xi Wang <wangxieric@gmail.com>
2024-12-02 00:33:46 +0000

    Update model_tuning.sh

--- model_tuning.sh
+++ model_tuning.sh
@@ -9,5 +9,5 @@ module load CUDA/12.4.0
 export HF_HOME="/mnt/parscratch/users/ac1xwa/huggingface"
 module load Anaconda3/2022.05
 source activate cogbias
-huggingface-cli login --token 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
+huggingface-cli login --token <token>
 python model_adapter_tuning.py
