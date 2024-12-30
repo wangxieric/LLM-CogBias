07f0515ecae0858a1d73baed5d03ab5d8663dac3
Xi Wang <wangxieric@gmail.com>
2024-12-30 14:35:31 +0000

    Update model_tuning_deepspeed.py

--- model_tuning_deepspeed.py
+++ model_tuning_deepspeed.py
@@ -1,4 +1,6 @@
 import os
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+
 from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments, Trainer
 from datasets import load_dataset
 
