bd115d59be348cb7a98162d88469f69ffbf8820e
Xi Wang <wangxieric@gmail.com>
2025-01-06 18:55:10 +0000

    Update data_tokenization.py

--- src/data_tokenization.py
+++ src/data_tokenization.py
@@ -2,7 +2,7 @@ from datasets import load_dataset
 import os
 from transformers import AutoTokenizer
 
-DATA_FILE = "/mnt/parscratch/users/ac1xwa/pythia/pre-train_data_csv/ArXiv_sampled.csv"  # Path to your text dataset
+DATA_FILE = "/mnt/parscratch/users/ac1xwa/pythia/pre-train_data_csv/Gutenberg.csv"  # Path to your text dataset
 dataset = load_dataset('csv', data_files=DATA_FILE, split='train')
 # sub_dataset = dataset.select(range(1000))
 
@@ -12,7 +12,7 @@ tokenizer = AutoTokenizer.from_pretrained(base_model)
 tokenizer.pad_token = tokenizer.eos_token
 
 def tokenize_function(examples):
-    tokenized = tokenizer(examples["text"], truncation=True, padding="max_length", max_length=1024)
+    tokenized = tokenizer(examples["text"], truncation=True, padding="max_length", max_length=512)
     tokenized["labels"] = tokenized["input_ids"][:]
     return tokenized
 
@@ -20,5 +20,5 @@ num_processes = os.cpu_count()
 tokenized_dataset = dataset.map(tokenize_function, batched=True, num_proc=2)
 
 # save the tokenized dataset
-TOKENISED_DATASET_PATH = "/mnt/parscratch/users/ac1xwa/pythia/pre-train_data_csv/tokenized_arxiv_sampled"
+TOKENISED_DATASET_PATH = "/mnt/parscratch/users/ac1xwa/pythia/pre-train_data_csv/tokenized_Gutenberg"
 tokenized_dataset.save_to_disk(TOKENISED_DATASET_PATH)
