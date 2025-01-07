474d8bfebd074d012f3a5a0d1e9764211e122b78
Xi Wang <wangxieric@gmail.com>
2025-01-07 22:06:58 +0000

    Update sample_data.py

--- src/sample_data.py
+++ src/sample_data.py
@@ -2,6 +2,7 @@ import csv
 import random
 from transformers import AutoTokenizer
 from datasets import load_dataset
+from datasets import concatenate_datasets
 
 def sample_instances_by_tokens(input_csv_path, output_csv_path, text_column, model_name, target_token_count):
     """
@@ -21,17 +22,25 @@ def sample_instances_by_tokens(input_csv_path, output_csv_path, text_column, mod
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
     # Read the CSV file and shuffle rows
-    dataset = load_dataset('csv', data_files=input_csv_path, split='train')
+    # if input_csv_path is a list, do concatenate
+    if isinstance(input_csv_path, list):
+        datasets = [] 
+        for data_path in input_csv_path:
+            dataset = load_dataset('csv', data_files=data_path, split='train')
+            datasets.append(dataset)
+        full_dataset = concatenate_datasets(datasets)
+    else:
+        full_dataset = load_dataset('csv', data_files=input_csv_path, split='train')
     # shuffle dataset
-    dataset = dataset.shuffle(seed=42)
+    full_dataset = full_dataset.shuffle(seed=42)
     sample_row_count = 0
     with open(output_csv_path, mode='w', encoding='utf-8') as output_file:
-        writer = csv.DictWriter(output_file, fieldnames=dataset.column_names)
+        writer = csv.DictWriter(output_file, fieldnames=full_dataset.column_names)
         writer.writeheader()
         
         # Sample rows until the target token count is reached or exceeded
         total_tokens = 0
-        for data in dataset:
+        for data in full_dataset:
             if text_column not in data:
                 raise ValueError(f"The specified column '{text_column}' does not exist in the CSV file.")
 
@@ -52,14 +61,14 @@ def sample_instances_by_tokens(input_csv_path, output_csv_path, text_column, mod
 if __name__ == "__main__":
     # Example usage
     data_path = "/mnt/parscratch/users/ac1xwa/pythia/pre-train_data_csv"
-    data_type = "FreeLaw"
-    input_csv_path = f"{data_path}/{data_type}.csv"  # Replace with the path to your CSV file
+    data_types = ["ArXiv", "NIH\ ExPorter", "PhilPapers"]
+    input_csv_path = [f"{data_path}/{data_type}.csv" for data_type in data_types]
     text_column = "text"  # Replace with the name of your text column
     model_name = "meta-llama/Meta-Llama-3-8B"  # Replace with the model you are using
     target_token_count = 13185228241  # Replace with your target token count
 
     try:
-        output_csv_path = f"{data_path}/legal_analyst.csv"
+        output_csv_path = f"{data_path}/scientific_scholar.csv"
         sample_instances_by_tokens(input_csv_path, output_csv_path, text_column, model_name, target_token_count)
         
     except Exception as e:
