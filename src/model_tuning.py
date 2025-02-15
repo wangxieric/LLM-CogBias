e41615e6080b55d061727346b366eea08980151b
Xi Wang <wangxieric@gmail.com>
2025-02-15 14:55:04 +0000

    Update model_tuning.py

--- src/model_tuning.py
+++ src/model_tuning.py
@@ -18,9 +18,10 @@ tokenizer = AutoTokenizer.from_pretrained(base_model)
 tokenizer.pad_token = tokenizer.eos_token
 model = AutoModelForCausalLM.from_pretrained(base_model)
 max_length = 512
+category = "scientific_mathematician"
 # data name
-DATA_NAME = "scientific_scholar" + "_" + str(max_length)
-OUTPUT_NAME = "scientific_scholar"
+DATA_NAME = category + "_" + str(max_length)
+OUTPUT_NAME = category
 
 batch_size = 10
 args = TrainingArguments(
