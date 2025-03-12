14fad4ea6bf6a13f54b8307c66c153b4573c2dde
Xi Wang <wangxieric@gmail.com>
2025-03-12 20:42:51 +0000

    Create evaluation_LM.sh

---
+++ run/evaluation_LM.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=300G
+#SBATCH --time=96:00:00
+
+module load CUDA/12.4.0
+export HF_HOME="/mnt/parscratch/users/ac1xwa/huggingface"
+module load Anaconda3/2022.05
+source activate cogbias
+huggingface-cli login --token <token>
+python src/evaluation_LM.py --model_name "XiWangEric/literary-classicist-llama3" --dataset_name "mmlu" --task "multi_subject_mc"
