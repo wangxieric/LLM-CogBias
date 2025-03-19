e2628263a765c7c09e278ec53d6a1795f6482b00
Xi Wang <wangxieric@gmail.com>
2025-03-19 21:48:13 +0000

    Update evaluation_LM.sh

--- run/evaluation_LM.sh
+++ run/evaluation_LM.sh
@@ -10,4 +10,4 @@ export HF_HOME="/mnt/parscratch/users/ac1xwa/huggingface"
 module load Anaconda3/2022.05
 source activate cogbias
 huggingface-cli login --token <token>
-python src/evaluation_LM.py --model_name "XiWangEric/literary-classicist-llama3" --dataset_name "mmlu" --task "multi_subject_mc"
+python src/eval_language_modelling.py --model_name "XiWangEric/literary-classicist-llama3" --dataset_name "mmlu" --task "multi_subject_mc"
